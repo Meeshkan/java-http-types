c6d2e5d6f2b76fe227024135c2a00163018d531e
Fredrik Fornwall <fredrik@fornwall.net>
2020-01-28 01:10:15 +0100

    Start json parsing

--- src/main/java/com/meeshkan/http/types/HttpExchange.java
+++ src/main/java/com/meeshkan/http/types/HttpExchange.java
@@ -1,5 +1,13 @@
 package com.meeshkan.http.types;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 /**
  * A HTTP request and response pair.
  */
@@ -19,6 +27,53 @@ public final class HttpExchange {
     private HttpExchange(HttpRequest request, HttpResponse response) {
         this.request = request;
         this.response = response;
+    }
+
+    public static HttpExchange fromJson(InputStream in) throws IOException {
+        StringBuilder buffer = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line).append('\n');
+        }
+
+        JSONObject jsonObject = new JSONObject(buffer.toString());
+        JSONObject requestObject = jsonObject.getJSONObject("request");
+        JSONObject responseObject = jsonObject.getJSONObject("response");
+
+        String methodString = requestObject.getString("method");
+        HttpMethod methodEnum = HttpMethod.valueOf(methodString.toUpperCase());
+
+        String protocolString = requestObject.getString("protocol");
+        HttpProtocol protocolEnum = HttpProtocol.valueOf(protocolString.toUpperCase());
+
+        HttpHeaders.Builder requestHeaders = new HttpHeaders.Builder();
+        JSONObject requestHeadersObject = requestObject.getJSONObject("headers");
+        for (String headerName : requestHeadersObject.keySet()) {
+            String headerValue = requestHeadersObject.getString(headerName);
+            requestHeaders.add(headerName, headerValue);
+        }
+
+        HttpRequest.Builder requestBuilder = new HttpRequest.Builder()
+                .setMethod(methodEnum)
+                .setHeaders(requestHeaders.build());
+
+        HttpHeaders.Builder responseHeaders = new HttpHeaders.Builder();
+        JSONObject responseHeadersObject = responseObject.getJSONObject("headers");
+        for (String headerName : responseHeadersObject.keySet()) {
+            String headerValue = responseHeadersObject.getString(headerName);
+            responseHeaders.add(headerName, headerValue);
+        }
+
+        HttpResponse.Builder responseBuilder = new HttpResponse.Builder()
+                .setStatusCode(responseObject.getInt("statusCode"))
+                .setHeaders(responseHeaders.build())
+                .setBody(responseObject.getString("body"));
+
+        return new HttpExchange.Builder()
+                .setRequest(requestBuilder.build())
+                .setResponse(responseBuilder.build())
+                .build();
     }
 
     public static class Builder {
--- src/test/java/com/meeshkan/http/types/HttpExchangeTest.java
+++ src/test/java/com/meeshkan/http/types/HttpExchangeTest.java
@@ -1,5 +1,7 @@
 package com.meeshkan.http.types;
 
+import org.junit.jupiter.api.Test;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -7,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpExchangeTest {
 
+    @Test
     void basicUsage() throws MalformedURLException {
         HttpExchange exchange = new HttpExchange.Builder()
                 .setRequest(new HttpRequest.Builder()
@@ -29,6 +32,17 @@ public class HttpExchangeTest {
         assertEquals("requestBody", exchange.getRequest().getBody());
         assertEquals("value", exchange.getRequest().getHeaders().getFirst("RequestHeader"));
         assertEquals("responseBody", exchange.getResponse().getBody());
+    }
+
+    @Test
+    void loadFromJson() throws Exception {
+        HttpExchange exchange = HttpExchange.fromJson(getClass().getResourceAsStream("/sample.json"));
+        assertEquals(HttpMethod.GET, exchange.getRequest().getMethod());
+        assertEquals("*/*", exchange.getRequest().getHeaders().getFirst("accept"));
+        assertEquals("Mozilla/5.0 (pc-x86_64-linux-gnu) Siege/3.0.8", exchange.getRequest().getHeaders().getFirst("user-agent"));
+
+        assertEquals(200, exchange.getResponse().getStatusCode());
+        assertEquals("1999", exchange.getResponse().getHeaders().getFirst("content-length"));
     }
 
 }
